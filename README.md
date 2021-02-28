37297f4c9137138bd889637ba5bfe572805fe9f9
justinmorales1 <jm.em0210@gmail.com>
2021-02-28 11:39:38 -0600

    Adding comments and a readMe for future use

---
+++ README.md
@@ -0,0 +1,17 @@
+# Getting Started with Create React App
+This project is just a simple express server that connects to a mongoDb.
+It has some simple routes to get and update collections in mongo.
+
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm run dev`
+
+Runs the app in the development mode.\
+Open [http://localhost:5000](http://localhost:5000) to view it in the browser.
+
+By running npm run dev you are using nodemon so you do not need
+to stop and re start your express server every time you make a change
+it will automatically do it for you. 
--- index.js
+++ index.js
@@ -3,26 +3,29 @@ const mongoose = require('mongoose');
 const keys = require('./config/dev');
 const bodyParser = require('body-parser');
 
-
 //When this file boots up this require lets mongoose know to handle the configurations
 require('./models/User');
 
-
+//You need a mongoId key to connect to your database. The one below is my own config key
+// that does not get added because its in a gitIgore file. You would need to get your own key
+// in order for this connection to work properly.
 mongoose.connect(keys.mongoId)
 
+//Sets up express server
 const app = express();
 
+//Pase the incoming request bodies in the middleware.
 app.use(
     bodyParser.json()
 );
 
-
+//Adds the userRoutes to express servers
 require('./routes/userRoutes')(app);
 
-
-
+//Just a dummy end point to test express server.
 app.get('/user', (req,res) => {
     res.send({hi: 'hello there'})
 });
 
+//You can use whatever port you like but I stuck to port 5000.
 app.listen(5000);
