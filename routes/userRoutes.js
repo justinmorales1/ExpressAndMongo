c0a68ac1f8ffd1cc16601bacc8316ee41041c0db
justinmorales1 <jm.em0210@gmail.com>
2021-02-28 11:29:16 -0600

    Updating user forms

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,14 +2,24 @@ const mongoose = require('mongoose');
 const User = mongoose.model('users');
 
 module.exports = app => {
-    //Create a new form
+    //Create a new form will following body
+    // {
+    //     "name": "Thomas Turpin",
+    //     "employeeId": 8675309,
+    //     "age": 32
+    // }
     app.post('/form/create', async (req, res) => {
         const user = await new User(req.body).save();
 
         res.send(user);
     });
 
-    //Retrieve a single
+    //Retrieve a single form with the following body
+    // {
+    //     "name": "Thomas Turpin",
+    //     "employeeId": 8675309,
+    //     "age": 32
+    // }
     app.get('/form/retrieve', async (req, res) => {
         const forms = await User.find(req.body);
 
@@ -23,7 +33,12 @@ module.exports = app => {
         res.send(forms);
     });
 
-    //Update a single form based off name
+    //Update a single form based off name from the following object
+    // {
+    //     "name": "Thomas Turpin",
+    //     "employeeId": 8675309,
+    //     "age": 32
+    // }
     app.post('/form/update', async (req, res) => {
         const forms = await User.updateOne({name:req.body.name},{$set:req.body});
 
