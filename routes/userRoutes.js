645497681f5c84f02adc7cd0da3e0a9a427259d7
justinmorales1 <jm.em0210@gmail.com>
2021-02-28 11:27:26 -0600

    Updating user forms

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,39 +2,32 @@ const mongoose = require('mongoose');
 const User = mongoose.model('users');
 
 module.exports = app => {
-    app.post('/new/forms', async (req, res) => {
-        console.log('The req is', req.body)
-
+    //Create a new form
+    app.post('/form/create', async (req, res) => {
         const user = await new User(req.body).save();
 
         res.send(user);
     });
 
-
-    app.get('/retrieve/forms', async (req, res) => {
-        console.log("The request is", req)
+    //Retrieve a single
+    app.get('/form/retrieve', async (req, res) => {
         const forms = await User.find(req.body);
 
         res.send(forms);
-
     });
 
-    app.get('/retrieve/all/forms', async (req, res) => {
+    //Retrieve all forms in User collection
+    app.get('/form/retrieve/all', async (req, res) => {
         const forms = await User.find();
 
         res.send(forms);
-
     });
 
-
+    //Update a single form based off name
     app.post('/form/update', async (req, res) => {
-        //const forms = await User.find();
-
-
         const forms = await User.updateOne({name:req.body.name},{$set:req.body});
 
         res.send(forms);
-
     });
 };
 
