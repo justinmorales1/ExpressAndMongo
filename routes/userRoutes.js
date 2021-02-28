07816044ab1b40eb6c170e4c169ab71ba07addf4
justinmorales1 <jm.em0210@gmail.com>
2021-02-28 08:21:02 -0600

    Setting up route to get a list of forms by a particular name

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,11 +2,22 @@ const mongoose = require('mongoose');
 const User = mongoose.model('users');
 
 module.exports = app => {
-    app.post('/users', async (req, res) => {
+    app.post('/new/forms', async (req, res) => {
         console.log('The req is', req.body)
 
         const user = await new User(req.body).save();
 
         res.send(user);
+    });
+};
+
+
+module.exports = app => {
+    app.get('/retrieve/forms', async (req, res) => {
+        console.log('The req is', req.body)
+        const forms = await User.find(req.body);
+
+        res.send(forms);
+
     });
 };
