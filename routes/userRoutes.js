532fd152fa22885f41f01b2cf71f01c717da625d
justinmorales1 <jm.em0210@gmail.com>
2021-02-28 08:49:55 -0600

    Adding route to update a forms age or employeeId

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,25 +9,32 @@ module.exports = app => {
 
         res.send(user);
     });
-};
 
 
-module.exports = app => {
     app.get('/retrieve/forms', async (req, res) => {
-
+        console.log("The request is", req)
         const forms = await User.find(req.body);
 
         res.send(forms);
 
     });
-};
 
-
-module.exports = app => {
     app.get('/retrieve/all/forms', async (req, res) => {
         const forms = await User.find();
 
         res.send(forms);
 
     });
+
+
+    app.post('/form/update', async (req, res) => {
+        //const forms = await User.find();
+
+
+        const forms = await User.updateOne({name:req.body.name},{$set:req.body});
+
+        res.send(forms);
+
+    });
 };
+
